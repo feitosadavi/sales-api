b4770a797c8c9ef60ceb348743e76838b794456f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-04-17 11:10:53 -0700

    refactor: add default option to port if is not in env

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,7 @@ import setupApp from './config/app';
 
 const app = setupApp();
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[SERVER] Running at http://localhost:${PORT}`);
 });
